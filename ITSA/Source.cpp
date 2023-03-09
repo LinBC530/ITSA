e7d5ee66402d287dd11f1330b168c72865e295ef
BNXX <bb086050@gmail.com>
2023-03-09 15:27:35 +0800

    success

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-
+	int miles;
+	double kilometer;
+	cin >> miles;
+	kilometer = (double) miles * 1.6;
+	cout << fixed << setprecision(1) << kilometer << endl;
 	return 0;
 }
