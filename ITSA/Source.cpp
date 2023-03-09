5a6b1159cf007b7d9b59d6633f61c18f91431fd4
BNXX <bb086050@gmail.com>
2023-03-09 21:20:28 +0800

    完成

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -6,12 +6,14 @@ int main()
 	int hour[2], minute[2], total;
 	cin >> hour[0] >> minute[0];
 	cin >> hour[1] >> minute[1];
+	//計算出總共停多少分鐘
 	total = (hour[1] - hour[0]) * 60 + minute[1] - minute[0];
+	//判斷分別停多久，分成三個區段，分別為120分鐘內(含120分鐘、120分鐘以上240分鐘以下、240分鐘以上(含240分鐘)
 	if (total <= 120)
-		cout << total/30*30 << endl;
-	else if (total > 120 and total <= 240)
-		cout << (total - 120) / 30 * 40 + 120 << endl;
-	else if (total > 240)
-		cout << (total - 240) / 30 * 60 + 280 << endl;
+		cout << (int)(total/30)*30 << endl;
+	else if (total > 120 and total < 240)
+		cout << (int)((total - 120) / 30) * 40 + 120 << endl;
+	else if (total >= 240)
+		cout << (int)((total - 240) / 30) * 60 + 280 << endl;
 	return 0;
 }
