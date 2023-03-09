a9e20e39b978973c606a8ee92ff413b28a1c764c
BNXX <bb086050@gmail.com>
2023-03-09 16:02:32 +0800

    加上註解

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -7,7 +7,9 @@ int main()
 	int x, y;
 	double distance;
 	cin >> x >> y;
+	//計算與中心點的距離並存入 distance (sqrt()為開根號的函數，pow()為平方的函數)
 	distance = sqrt(pow(x, 2) + pow(y, 2));
+	//當 distance 於半徑內(含等於)輸出 inside ，否則輸出 outside
 	if (distance <= 100)
 		cout << "inside" << endl;
 	else
