c0e175223191ed1b2d201a367792ace54c45246f
BNXX <bb086050@gmail.com>
2023-03-09 15:57:42 +0800

    成功

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-
+	int x, y;
+	double distance;
+	cin >> x >> y;
+	distance = sqrt(pow(x, 2) + pow(y, 2));
+	if (distance <= 100)
+		cout << "inside" << endl;
+	else
+		cout << "outside" << endl;
 	return 0;
 }
