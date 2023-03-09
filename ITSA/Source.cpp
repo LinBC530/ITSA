e32779c2545d3a366cf35da54656e924f6042ee0
BNXX <bb086050@gmail.com>
2023-03-09 22:31:25 +0800

    完成

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
 int main()
 {
-
+	int input;
+	cin >> input;
+	//將input轉成2進位至bit
+	bitset<8> bit(input);
+	cout << bit << endl;
 	return 0;
 }
