f4c2e62d8c8027888be1604e3c2265f21cd655b8
BNXX <bb086050@gmail.com>
2023-03-17 01:19:24 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-
+	int input;
+	cin >> input;
+    if (input <= 1) {
+        cout << "NO" << endl;
+    }
+    for (int i = 2; i * i <= input; i++) {
+        if (input % i == 0) {
+            cout << "NO" << endl;
+        }
+    }
+    cout << "YES" << endl;
 	return 0;
 }
