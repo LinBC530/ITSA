ee409fa060f16e9aecc93edef3f268286db4b3e7
BNXX <bb086050@gmail.com>
2023-03-17 01:31:00 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-
+	int sum = 0,input;
+	cin >> input;
+	for (int num = 3; num <= input; num++)
+	{
+		if (num % 3 == 0)
+		{
+			sum += num;
+		}
+	}
+	cout << sum << endl;
 	return 0;
 }
