dce9b57dc7c255daa0c5eb7d93c4a4edf6388d68
BNXX <bb086050@gmail.com>
2023-03-20 00:58:12 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int RC(int n)
+{
+	if (n > 1)
+	{
+		return RC(n - 1) + RC(n / 2);
+	}
+	else if (n == 0 or n == 1)
+	{
+		return n + 1;
+	}
+}
+
 int main()
 {
-
+	int n;
+	cin >> n;
+	cout << RC(n) << endl;
 	return 0;
 }
