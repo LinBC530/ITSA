be9ba8e5adb18867d285fb610cf64c6b071b4154
BNXX <bb086050@gmail.com>
2023-03-20 00:26:23 +0800

    加上註解

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//動態配置記憶體
 int** Matrix(int n, int m)
 {
 	int** matrix = new int*[n];
@@ -9,6 +10,7 @@ int** Matrix(int n, int m)
 	return matrix;
 }
 
+//釋放記憶體空間
 void Freed(int** &matrix,int n)
 {
 	for(int i=0;i<n;i++)
@@ -19,6 +21,8 @@ void Freed(int** &matrix,int n)
 	delete[] matrix;
 }
 
+
+//反轉矩陣
 void Reverse(int** &matrix,int** &R_matrix,int n,int m)
 {
 	for (int i = 0; i < n; i++)
@@ -30,6 +34,7 @@ void Reverse(int** &matrix,int** &R_matrix,int n,int m)
 	}
 }
 
+//輸入矩陣內容
 void Input(int** &matrix, int n, int m)
 {
 	for (int i = 0; i < n; i++)
@@ -41,6 +46,7 @@ void Input(int** &matrix, int n, int m)
 	}
 }
 
+//輸出矩陣內容
 void Output(int** &matrix, int n, int m)
 {
 	for (int i = 0; i < n; i++)
@@ -59,11 +65,21 @@ int main()
 {
 	int n, m;
 	cin >> n >> m;
+
+	//使用動態配置記憶體，建立存放矩陣內容的二維陣列
 	int** data = Matrix(n, m);
 	int** ans = Matrix(m, n);
+
+	//輸入矩陣內容存入data
 	Input(data,n,m);
+
+	//反轉data的矩陣內容存入ans
 	Reverse(data, ans,n,m);
+
+	//輸出反轉後矩陣ans
 	Output(ans,m,n);
+
+	//釋放data及ans二維陣列記憶體空間
 	Freed(data,n);
 	Freed(ans,m);
 	return 0;
