cb6ef0baad7bcbdef7e678964b7d4a65f8952171
BNXX <bb086050@gmail.com>
2023-03-20 03:00:04 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-
+	string data;
+	cin >> data;
+	int size = data.length(),L,R;
+	L = (size/2)-1;
+	R = size / 2;
+	if (data.length() % 2 != 0) { R++; }
+	for (int i = L; i >= 0; i--)
+		if ((data.at(i) == data.at(R++)) == 0)
+		{
+			cout << "NO" << endl;
+			exit(0);
+		}
+	cout << "YES" << endl;
 	return 0;
 }
