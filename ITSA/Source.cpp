594bebb782c8a610959328875d13e3ca0ec74bee
BNXX <bb086050@gmail.com>
2023-04-16 23:00:11 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-
+	bool same = false;
+	int num = 0;
+	string str, data;
+	cin >> str >> data;
+	for (int i = 0; i < data.length() - str.length() + 1; i++)
+	{
+		if (data[i] == str[0])
+		{
+			same = true;
+			for (int j = 0,k = i; j < str.length(); j++,k++)
+			{
+				if (data[k] != str[j])
+				{
+					same = false;
+					break;
+				}
+			}
+			if (same == true)
+			{
+				num++;
+				same = false;
+			}
+		}
+	}
+	cout << num << endl;
 	return 0;
 }
