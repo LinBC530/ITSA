bbc4e6ddbdccbc5a28d8745d64736042f4e9246f
BNXX <bb086050@gmail.com>
2023-04-16 21:31:23 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-
+	string str;
+	getline(cin, str);
+	int num=1,ans[26][2]{0};
+	for (int i = 0, add = 97; i < 26; i++, add++)
+		ans[i][0] = add;
+	for (int i = 0; i < str.length(); i++)
+	{
+		for (int j = 0; j < 26; j++)
+		{
+			if (str[i] == ' ')
+			{
+				num++;
+				break;
+			}
+			else if(str[i] == ',' || str[i] == '.')
+				break;
+			else if (str[i] == (char)ans[j][0] || str[i] == (char)(ans[j][0] - 32))
+			{
+				ans[j][1]++;
+			}
+		}
+	}
+	cout << num << endl;
+	for (int i = 0; i < 26; i++)
+	{
+		if (ans[i][1] == 0)
+			continue;
+		else
+			cout << (char)ans[i][0] << " : " << ans[i][1] << endl;
+	}
 	return 0;
 }
