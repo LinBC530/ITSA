3b9ceef9078370bc11cce5cc490f3c761d14b3b7
BNXX <bb086050@gmail.com>
2023-04-19 11:19:30 +0800

    ok

--- ITSA/Source.cpp
+++ ITSA/Source.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <string>
+#include <list>
 using namespace std;
 
 int main()
 {
-
+	string str;
+	list<string> data;
+	while (cin >> str)
+	{
+		for (int i = 0; i < str.length(); i++)
+		{
+			if ((int)str[i] >= 65 and (int)str[i] <= 90)
+				str[i] = (int)str[i] + 32;
+		}
+		if (data.empty()){}
+		else
+		{
+			for (string i : data)
+			{
+				if (str == i)
+					goto check_enter;
+			}
+		}
+		data.push_back(str);
+	check_enter:
+		if (getchar() == '\n')
+			break;
+	}
+	for (string i : data)
+	{
+		if(i != data.back())
+			cout << i << " ";
+		else
+			cout << i << endl;
+	}
 	return 0;
 }
